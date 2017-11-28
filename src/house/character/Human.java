b69f0a5c7b9e2d339d95a34a6df313acee55f124
m <mahhov1@gmail.com>
2017-11-28 07:27:11 -0800

    recoloring human & monster

--- src/house/character/Human.java
+++ src/house/character/Human.java
@@ -8,7 +8,7 @@ import java.awt.*;
 
 public class Human extends Character {
     private static final double SMELL_DISTANCE = 3, SOUND_WALK_DISTANCE = 9, SOUND_RUN_DISTANCE = 27;
-    private static final Color COLOR_TOP = new Color(0, 80, 0), COLOR_SIDE = new Color(0, 120, 0);
+    private static final Color COLOR_TOP = new Color(0, 120, 0), COLOR_SIDE = new Color(0, 80, 0);
     private static final double WALK_SPEED = .1, RUN_SPEED = .3;
     private boolean light;
 
--- src/house/character/Monster.java
+++ src/house/character/Monster.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class Monster extends Character {
     private static final double SMELL_DISTANCE = 3, SOUND_WALK_DISTANCE = 9, SOUND_RUN_DISTANCE = 27;
-    private static final Color COLOR_TOP = new Color(0, 80, 0), COLOR_SIDE = new Color(0, 120, 0);
+    private static final Color COLOR_TOP = new Color(160, 0, 0), COLOR_SIDE = new Color(120, 0, 0);
     private static final double WALK_SPEED = .15, RUN_SPEED = .45;
 
     public Monster(double x, double y) {
