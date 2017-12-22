7da040f4d6a62b034ae1376537f3de9b072f56de
m <mahhov1@gmail.com>
2017-12-22 08:40:54 -0800

    minor refactor drawUtil for duplicated code and add draw rect from center

--- src/util/DrawUtil.java
+++ src/util/DrawUtil.java
@@ -14,6 +14,10 @@ public class DrawUtil {
         painterQueue.add(new PainterPolygon(geometry.getRect(), light, color, false, true), layer);
     }
 
+    public static void drawRectFromCenter(PainterQueue painterQueue, Camera camera, double x, double y, double size, double light, Color color, int layer) {
+        drawRectFromCorner(painterQueue, camera, x - size * .5, y - size * .5, size, light, color, layer);
+    }
+
     public static void drawCubeFromCorner(PainterQueue painterQueue, Camera camera, double left, double top, double size, double light, Color colorTop, Color colorSide, int topLayer, int sideLayer) {
         CubeGeometry geometry = new CubeGeometry(left, top, 0, size, size, size, camera);
         painterQueue.add(new PainterPolygon(geometry.getTop(), light, colorTop, false, true), topLayer);
@@ -23,12 +27,7 @@ public class DrawUtil {
         painterQueue.add(new PainterPolygon(geometry.getLeft(), light, colorSide, false, true), sideLayer);
     }
 
-    public static void drawCubeFromCenter(PainterQueue painterQueue, Camera camera, double x, double y, double size, Color colorTop, Color colorSide, int topLayer, int sideLayer) {
-        CubeGeometry geometry = new CubeGeometry(x - size * .5, y - size * .5, 0, size, size, size, camera);
-        painterQueue.add(new PainterPolygon(geometry.getTop(), 1, colorTop, false, true), topLayer);
-        painterQueue.add(new PainterPolygon(geometry.getFront(), 1, colorSide, false, true), sideLayer);
-        painterQueue.add(new PainterPolygon(geometry.getRight(), 1, colorSide, false, true), sideLayer);
-        painterQueue.add(new PainterPolygon(geometry.getBack(), 1, colorSide, false, true), sideLayer);
-        painterQueue.add(new PainterPolygon(geometry.getLeft(), 1, colorSide, false, true), sideLayer);
+    public static void drawCubeFromCenter(PainterQueue painterQueue, Camera camera, double x, double y, double size, double light, Color colorTop, Color colorSide, int topLayer, int sideLayer) {
+        drawCubeFromCorner(painterQueue, camera, x - size * .5, y - size * .5, size, light, colorTop, colorSide, topLayer, sideLayer);
     }
 }
